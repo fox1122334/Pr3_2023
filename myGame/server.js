c86e437d29cb0973e6705395ab5862f809080741
fox1122334 <fox.123@tumo.world>
2024-01-09 19:53:53 +0100

    server Datei geupdated noch nicht fertig

--- myGame/server.js
+++ myGame/server.js
@@ -1,22 +1,47 @@
-
 const Grass = require("./classes/grass.js");
 const Grazer = require("./classes/grazer.js");
 const Predator = require("./classes/predator.js");
 
 const express = require("express");
+const { ClientRequest } = require("http"); 
 const app = express();
+
+app.use(express.static("./client"))
+
+app.get("/", function(req, res) {
+    res.redirect("index.html")
+});
 
 let server = require('http').Server(app);
 let io = require('socket.io')(server);
 
-server.listen(3000, function (){
+server.listen(3000, function(){
     console.log("Der Server läuft auf port 3000...");
-    // initGame();
-    // setInterval(function(){
-    //     updateGame();
-    // }, 1000);
-   
-});
+
+    io.on("connection", function (socket) {
+        console.log("ws connection established...");
+        clients.push(socket.id);
+        //Spielstart
+        if (clients.length == 1 && isGameRunning == false) {
+            console.log("Starte Spiel... wenn noch nicht gestartet...")
+            initGame()
+            interVaLID = setInterval(updateGame, 1000);
+            isGameRunning = true
+        }
+        socket.on("disconnect", function() {
+            console.log("client left...");
+            const foundIndex = clients.findIndex(id =>id === socket.id);
+            if (foundIndex >= 0){
+                clients.splice(foundIndex, 1)
+            }
+            if (clients.length === 0){
+                isGameRunning = false;
+                clearInterval(interVaLID);
+                console.log("Spiel gestopt: keine Clients", clients.length)
+            }
+        })
+    }
+}
 
 // game logic on server
 // matrix = [
